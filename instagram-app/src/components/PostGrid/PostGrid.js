85c33d457b4976ed06111208de7ef04eb32f04e9
Moses Samuel <mosamuel98@gmail.com>
2019-05-01 10:57:18 -0700

    delete unnecessary console.log

--- instagram-app/src/components/PostGrid/PostGrid.js
+++ instagram-app/src/components/PostGrid/PostGrid.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import PostContainer from "../PostContainer/PostContainer"
 
 export default function PostGrid(props){
+    console.log(props)
     return props.posts.map(PostContainer)
 }
 
