2c81a8a4f44a8e1396af95cede17da5d3a6ea846
Moses Samuel <mosamuel98@gmail.com>
2019-05-01 12:04:59 -0700

    add new action creator for adding posts

--- instagram-app/src/lib/actionCreators.js
+++ instagram-app/src/lib/actionCreators.js
@@ -1,1 +1,19 @@
+import C from './constants'
+
 // TODO: add action creators here
+export const addComment = ({user, text, createdAt}) => ({ 
+    type: C.ADD_COMMENT,
+    payload: { user, text, createdAt}
+})
+
+export const addPost = ({
+    id, 
+    userName, 
+    thumbnailUrl, 
+    imageUrl, 
+    likes, 
+    timeStamp
+}) => ({
+    type: C.ADD_POST,
+    payload: { id, userName, thumbnailUrl, imageUrl, likes, timeStamp}
+})
