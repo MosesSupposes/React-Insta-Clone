87549cc34b260ca151803190d02919089abd2f73
Moses Samuel <mosamuel98@gmail.com>
2019-05-02 13:12:00 -0700

    add action creator for editing comments

--- instagram-app/src/lib/actionCreators.js
+++ instagram-app/src/lib/actionCreators.js
@@ -1,14 +1,26 @@
 import C from './constants'
 
+// ---- Comments ----
+
+
 export const addComment = ({postId, username, text, createdAt}) => ({ 
     type: C.ADD_COMMENT,
     payload: { postId, username, text, createdAt }
+})
+
+export const editComment = ({postId, index, text, author}) => ({
+    type: C.EDIT_COMMENT,
+    payload: {postId, index, text, author}
 })
 
 export const deleteComment = ({postId, index, author}) => ({
     type: C.DELETE_COMMENT,
     payload: { postId, index, author }
 })
+
+
+
+// ---- Posts ----
 
 export const addPost = ({
     id, 
